fc277fb43738e9d6083c596bfd1de39dbf1376fa
SerggioC <sergiocrz@gmail.com>
2018-04-30 19:13:50 +0100

    intent verification intended

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -1,39 +1,84 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.teatime;
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
+
+// DONE (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(JUnit4.class)
 public class OrderSummaryActivityTest {
 
-    // TODO (2) Add the rule that indicates we want to use Espresso-Intents APIs in functional UI tests
+    String email_message;
 
+    // DONE (2) Add the rule that indicates we want to use Espresso-Intents APIs
+    // in functional UI tests (IntentsTestRule)
+    @Rule
+    public IntentsTestRule<OrderSummaryActivity> mActivityRule
+            = new IntentsTestRule<>(OrderSummaryActivity.class);
 
-    // TODO (3) Finish this method which runs before each test and will stub all external
+    // DONE (3) Finish this method which runs before each test and will stub all external
     // intents so all external intents will be blocked
-
+    @Before
     public void stubAllExternalIntents() {
-
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
     }
 
+    @Before
+    public void getStrings() {
+        Context context = getInstrumentation().getTargetContext();
+        Resources resources = context.getResources();
+        email_message = resources.getString(R.string.email_message);
+    }
 
-    // TODO (4) Finish this method which verifies that the intent sent by clicking the send email
+    // DONE (4) Finish this method which verifies that the intent sent by clicking the send email
     // button matches the intent sent by the application
-
+    @Test
     public void clickSendEmailButton_SendsEmail() {
-
+        onView(withId(R.id.send_email_button)).perform(click());
+        // intended(Matcher<Intent> matcher) asserts the given matcher matches one and only one
+        // intent sent by the application.
+        intended(allOf(
+                hasAction(Intent.ACTION_SENDTO),
+                hasExtra(Intent.EXTRA_TEXT, email_message))
+        );
     }
 }
--- app/src/main/java/com/example/android/teatime/OrderSummaryActivity.java
+++ app/src/main/java/com/example/android/teatime/OrderSummaryActivity.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.teatime;
 
 import android.content.Intent;
@@ -31,7 +31,7 @@ public class OrderSummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_summary);
-        Toolbar menuToolbar = (Toolbar) findViewById(R.id.order_summary_toolbar);
+        Toolbar menuToolbar = findViewById(R.id.order_summary_toolbar);
         setSupportActionBar(menuToolbar);
         getSupportActionBar().setTitle(getString(R.string.order_summary_title));
 
@@ -56,36 +56,30 @@ public class OrderSummaryActivity extends AppCompatActivity {
      * @param sugarType amount of sugar to add
      */
     private void displayOrderSummary(String teaName, int price, String size, String milkType,
-            String sugarType, int quantity) {
+                                     String sugarType, int quantity) {
 
         // Set tea name in order summary
-        TextView teaNameTextView = (TextView) findViewById(
-                R.id.summary_tea_name);
+        TextView teaNameTextView = findViewById(R.id.summary_tea_name);
         teaNameTextView.setText(teaName);
 
         // Set quantity in order summary
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.summary_quantity);
+        TextView quantityTextView = findViewById(R.id.summary_quantity);
         quantityTextView.setText(String.valueOf(quantity));
 
         // Set tea size in order summary
-        TextView sizeTextView = (TextView) findViewById(
-                R.id.summary_tea_size);
+        TextView sizeTextView = findViewById(R.id.summary_tea_size);
         sizeTextView.setText(size);
 
         // Set milk type in order summary
-        TextView milkTextView = (TextView) findViewById(
-                R.id.summary_milk_type);
+        TextView milkTextView = findViewById(R.id.summary_milk_type);
         milkTextView.setText(milkType);
 
         // Set sugar amount in order summary
-        TextView sugarTextView = (TextView) findViewById(
-                R.id.summary_sugar_amount);
+        TextView sugarTextView = findViewById(R.id.summary_sugar_amount);
         sugarTextView.setText(sugarType);
 
         // Set total price in order summary
-        TextView priceTextView = (TextView) findViewById(
-                R.id.summary_total_price);
+        TextView priceTextView = findViewById(R.id.summary_total_price);
 
         String convertPrice = NumberFormat.getCurrencyInstance().format(price);
         priceTextView.setText(convertPrice);
